5bdc0a3c2a60453d888bacd1c48e6429d9733683
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2025-09-04 00:41:37 -0300

    fix: add find by uuid

--- src/main/java/com/rhconnect/collaborators/repository/OccupationRepository.java
+++ src/main/java/com/rhconnect/collaborators/repository/OccupationRepository.java
@@ -4,6 +4,10 @@ import com.rhconnect.collaborators.model.Occupation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface OccupationRepository extends JpaRepository<Occupation, Long> {
+    Optional<Object> findById(UUID occupationId);
 }
